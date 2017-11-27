9f91d1c01fdf0fb56a7e000afb5d96d6a97a6ceb
Amen <362961910@qq.com>
2017-11-27 17:07:00 +0800

    添加swagger依赖并使用

--- security-demo/src/main/java/com/jy/study/spring/security/controller/UserController.java
+++ security-demo/src/main/java/com/jy/study/spring/security/controller/UserController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.jy.study.spring.security.dto.User;
 import com.jy.study.spring.security.dto.UserQueryCondition;
 import com.jy.study.spring.security.exception.UserNotException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +21,7 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    @ApiOperation("用户查询服务")
     @JsonView(User.UserSimpleView.class)
     @GetMapping
     public List<User> query(UserQueryCondition condition, @PageableDefault(page = 1, size = 5, sort = "username,desc") Pageable pageable) {
@@ -35,7 +38,7 @@ public class UserController {
     @JsonView(User.UserDetailView.class)
     //正则限制: 只能接受一个数字
     @GetMapping(value = "/{id:\\d+}")
-    public User getInfo(@PathVariable Long id) {
+    public User getInfo(@ApiParam("用户id") @PathVariable Long id) {
         User user = new User();
         user.setId(id);
         user.setUsername("tom");
